45820f39d6d743fe59a0eee75abe7b5acef322cb
Karthik-0-6 <karthisundarakumar@gmail.com>
2026-06-03 23:01:39 +0530

    Admin Layout changed in authentication

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -2,15 +2,50 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
+
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ('email', 'name', 'role', 'auth_provider', 'is_staff', 'is_active')
-    list_filter = ('role', 'auth_provider', 'is_staff', 'is_active')
+    list_display = (
+        'email', 'name', 'role',
+        'auth_provider', 'is_staff', 'is_active'
+    )
+
+    list_filter = (
+        'role', 'auth_provider',
+        'is_staff', 'is_active'
+    )
+
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal Info', {'fields': ('name', 'avatar')}),
-        ('Permissions', {'fields': ('role', 'auth_provider', 'is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions')}),
+        ('Permissions', {
+            'fields': (
+                'role',
+                'auth_provider',
+                'is_staff',
+                'is_superuser',
+                'is_active',
+                'groups',
+                'user_permissions',
+            )
+        }),
     )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'name',
+                'password1',
+                'password2',
+                'role',
+                'auth_provider',
+                'is_staff',
+                'is_active',
+            ),
+        }),
+    )
+
     search_fields = ('email', 'name')
     ordering = ('email',)
-
-admin.site.register(User, UserAdmin)
